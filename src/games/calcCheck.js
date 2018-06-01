729bc91c323e6dcf2633d76bc901425b6283c64e
Kasmenai <kasmenai@gmail.com>
2018-06-01 21:54:08 +0300

    refactor: Генерация вопроса  ответа объеденены в 1 switch в calcCheck.js

--- src/games/calcCheck.js
+++ src/games/calcCheck.js
@@ -4,39 +4,19 @@ import playGame from '..';
 
 const description = 'What is the result of the expression?';
 
-const getRandomOperation = () => {
-  switch (getRandomNum(0, 3)) {
-    case 0:
-      return '+';
-    case 1:
-      return '-';
-    case 2:
-      return '*';
-    default:
-      return '+';
-  }
-};
-
-const calcResult = (num1, operation, num2) => {
-  switch (operation) {
-    case '+':
-      return num1 + num2;
-    case '-':
-      return num1 - num2;
-    case '*':
-      return num1 * num2;
-    default:
-      return num1 + num2;
-  }
-};
-
 const answerQuestionGenerator = () => {
   const firstNum = getRandomNum(1, 5);
   const secondNum = getRandomNum(1, 10);
-  const operation = getRandomOperation();
-  const question = `${firstNum} ${operation} ${secondNum}`;
-  const rightAnswer = String(calcResult(firstNum, operation, secondNum));
-  return cons(question, rightAnswer);
+  switch (getRandomNum(0, 3)) {
+    case 0:
+      return cons(`${firstNum} + ${secondNum}`, `${firstNum + secondNum}`);
+    case 1:
+      return cons(`${firstNum} - ${secondNum}`, `${firstNum - secondNum}`);
+    case 2:
+      return cons(`${firstNum} * ${secondNum}`, `${firstNum * secondNum}`);
+    default:
+      return cons(`${firstNum} + ${secondNum}`, `${firstNum + secondNum}`);
+  }
 };
 
 export default () => playGame(description, answerQuestionGenerator);
