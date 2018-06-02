2401e424ac2ebfa0c4a8543c43298e608613144a
Kasmenai <kasmenai@gmail.com>
2018-06-02 16:52:34 +0300

    fix: Переименование progressGenerator -> getProgressionItem

--- src/games/progression.js
+++ src/games/progression.js
@@ -6,17 +6,17 @@ const description = 'What number is missing in this progression?';
 
 const elementsCount = 10;
 
-const progressGenerator = (firstNum, delta, i) => firstNum + ((i - 1) * delta);
+const getProgressionItem = (firstNum, delta, i) => firstNum + ((i - 1) * delta);
 
 const answerQuestionGenerator = () => {
   const firstNum = getRandomNum(1, 50);
   const delta = getRandomNum(1, 10);
-  const hiddenIndex = getRandomNum(2, 10);
+  const hiddenIndex = getRandomNum(2, elementsCount);
   let question = `${firstNum}`;
   for (let i = 2; i <= elementsCount; i += 1) {
-    question += i === hiddenIndex ? ' ..' : ` ${progressGenerator(firstNum, delta, i)}`;
+    question += i === hiddenIndex ? ' ..' : ` ${getProgressionItem(firstNum, delta, i)}`;
   }
-  const rightAnswer = `${progressGenerator(firstNum, delta, hiddenIndex)}`;
+  const rightAnswer = `${getProgressionItem(firstNum, delta, hiddenIndex)}`;
   return cons(question, rightAnswer);
 };
 
